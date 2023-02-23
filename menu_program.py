db222e57596d8206c3add5292710690e32630a8b
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-02-23 11:43:56 -0600

    Add files via upload

---
+++ menu_program.py
@@ -0,0 +1,14 @@
+ps = 1
+xbox = 2
+switch = 3
+oculus_vr = 4
+pc = 5
+
+
+def main():
+    print("Select only one video game platform that is your favorite:")
+    print("1.PlayStation", "\n2.Xbox", "\n3.Switch", "\n4.Oculus Quest", "\n5.PC")
+    int(input("Enter Number 1-5?"))
+
+
+main()
