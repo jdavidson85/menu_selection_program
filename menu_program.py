4e18165a20ebdf3c8cdbfdb59ab2c2b4dfe814af
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-02-23 12:02:38 -0600

    Add files via upload

--- menu_program.py
+++ menu_program.py
@@ -1,14 +1,51 @@
-ps = 1
-xbox = 2
-switch = 3
-oculus_vr = 4
-pc = 5
 
 
 def main():
     print("Select only one video game platform that is your favorite:")
-    print("1.PlayStation", "\n2.Xbox", "\n3.Switch", "\n4.Oculus Quest", "\n5.PC")
-    int(input("Enter Number 1-5?"))
+    print("1. PlayStation")
+    print("2. Xbox")
+    print("3. Switch")
+    print("4. Oculus")
+    print("5. PC")
+    choice = input("Please select number 1-5: ")
+
+    if choice == '1':
+        ps()
+    elif choice == '2':
+        xbox()
+    elif choice == '3':
+        switch()
+    elif choice == '4':
+        oculus()
+    elif choice == '5':
+        pc()
+    else:
+        print("That isn't a valid choice")
+
+
+def ps():
+    print("PlayStation is a great choice.")
+    print("Sony makes some unbelievable things.")
+
+
+def xbox():
+    print("Xbox is a great choice.")
+    print("Microsoft creates amazing things.")
+
+
+def switch():
+    print("Nintendo switch is so much fun.")
+    print("Nintendo has been making systems for almost 40 years")
+
+
+def oculus():
+    print("Oculus Quest 2 is one of the best VR headsets around.")
+    print("Facebook helped develop this VR headset.")
+
+
+def pc():
+    print("PC gaming is the new craze these days.")
+    print("Pc games have come along way since the early days.")
 
 
 main()
